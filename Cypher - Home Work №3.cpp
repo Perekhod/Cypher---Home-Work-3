079755e07f802fe636de65a82a71b591bf0d0c33
Perekhod <ilya.perekhod@gmail.com>
2023-02-14 02:59:48 +0500

    added encryption function

--- Cypher - Home Work №3.cpp
+++ Cypher - Home Work №3.cpp
@@ -8,32 +8,59 @@ int main()
 	std::cout << "Hello! I'm encryption machine by Perekhod v0.1" << std::endl;
 	std::cout << "Are you interested in encryption or decryption?" << std::endl;
 
-	repeat://11 and 43 line, if the user wants to go back to the beginning
+	repeat://11 and 75 line,return if the user wants to go back to the beginning.
+	jump:  //14 and 29 line,return if entered an invalid character.
 
 	std::cout << "To encrypt enter '+' to decrypt '-'" << std::endl;
 
 	char x;
 	std::cin >> x;
-	char cypher = 43;
-	char decryption = 45;
+	char encrypt = 43;
+	char decrypt = 45;
 
-	if (x == cypher)
-		std::cout << "Okay. You choosed " << cypher << std::endl;
-	else if (x == decryption)
-		std::cout << "Okay. You choosed " << decryption << std::endl;
+	if (x == encrypt)
+		std::cout << "Okay. You choosed " << encrypt << std::endl;
+	else if (x == decrypt)
+		std::cout << "Okay. You choosed " << decrypt << std::endl;
 	else
 	{
 		std::cout << "You entered an invalid character" << std::endl;
-		/*goto invalidchar;*/
+		if (x != encrypt && x != decrypt)
+			goto jump;
 	}
 
+	linesize://32 and 42 line, return if entered wrong number of characters
 	std::cout << "Enter line size:" << std::endl;
 	int size;
 	std::cin >> size;
-	std::cout << "Enter the string:" << std::endl;
+
+	if (std::cin.fail()) //checking for the correct number of characters in linesize
+	{
+		std::cout << "Incorrect input." << std::endl;
+		std::cin.clear();
+		while (std::cin.get()!='\n');
+		goto linesize;
+	}
+
+	std::cout << "Enter a string of " << size << " characters." << std::endl;
+
 	char arr[]{""};
 
-	if (x == cypher)
+	if (x == encrypt)
+	{
+		for (int i = 0; i < size; ++i)
+		{
+			std::cin >> arr[i]; //enter symbol
+			
+		}
+
+		for (int i = 0; i < size; ++i)
+		{
+			std::cout << arr[i]; // print symbol's
+		}
+			std::cout << std::endl;
+	}
+	else if(x == decrypt)
 	{
 		for (int i = 0; i < size; ++i)
 		{
@@ -42,32 +69,36 @@ int main()
 
 		for (int i = 0; i < size; ++i)
 		{
-			std::cout << arr[i] << ","; // print symbol's
+			std::cout << arr[i]; // print symbol's
 		}
+			std::cout << std::endl;
 	}
 
-	else if(x == decryption)
+	int sl;// shear length
+	std::cout << "Enter encryption integer 'X' =\n";
+	std::cin >> sl;
+
+	for (int i = 0; i < size; ++i)
 	{
-		for (int i = 0; i < size; ++i)
+		if (arr[i] == 'z')
 		{
-			std::cin >> arr[i]; //enter symbol
+			arr[i] = 'a';
 		}
-
-		for (int i = 0; i < size; ++i)
+		else
 		{
-			std::cout << arr[i] << ","; // print symbol's
+			arr[i] += sl;
+			std::sin.clear();
 		}
 	}
-	
+	std::cout << "Encryption result = " << arr << std::endl;
 
-	///*invalidchar:*/// 27 and 30 line, if enter not '+' or '-'.
 	bool _true = true;
 	while (_true = true)
 	{
 		std::cout << "Return to the beginning or close the program?" << std::endl;
 		std::cout << "Enter 'r' to retern or 'q' to exit the program" << std::endl;
 
-		char choice;
+		char choice;//selection variable 'return'or exit
 		std::cin >> choice;
 		switch (choice)
 		{
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-#12.02.23 - added "for" loops for reading and displaying the length of an array. Problem with "goto" not solved
+#12.02.23 - Rename variables 'cypher = encrypt' and 'decryption = decrypt'.
+#delete ''goto invalidchar', added 'goto jump' which returns to the beginning if entered an invalid character.
 
-#12.02.23 - Started work
-
-#Cypher - Home Work №3
+#12.02.23 - added "for" loops for reading and displaying the length of an array. Problem with "goto" not solved.
+#12.02.23 - Started work.
+#Cypher - Home Work №3.
